27e65c1d25373742c2e06e4cfaa84322287658e3
starsky1391 <bt47_starsky@outlook.com>
2026-03-10 17:44:19 +0800

    修复依赖冲突与部署问题

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,6 +11,9 @@ export const config = {
     iceServers: [
       {
         urls: 'stun:stun.l.google.com:19302'
+      },
+      {
+        urls: 'stun:stun1.l.google.com:19302'
       }
       // 可选：添加 TURN 服务器配置
       // {
